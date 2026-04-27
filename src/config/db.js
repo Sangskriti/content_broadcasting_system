ab61c9cba56a9407793aaaabecae2fcb94abacae
Sulagna <dola2000sulagna@gmail.com>
2026-04-28 01:46:09 +0530

    update the db code

--- src/config/db.js
+++ src/config/db.js
@@ -2,17 +2,17 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-    process.env.DB_NAME, 
-    process.env.DB_USER, 
+    process.env.DB_NAME || 'railway', 
+    process.env.DB_USER || 'root', 
     process.env.DB_PASS, 
     {
         host: process.env.DB_HOST,
-        port: process.env.DB_PORT || 46601, 
+        port: 46601, 
         dialect: 'mysql',
         logging: false,
         dialectOptions: {
             ssl: {
-                rejectUnauthorized: false 
+                rejectUnauthorized: false
             }
         }
     }
